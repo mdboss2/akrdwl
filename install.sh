f8b069c6d2bffb66923a6081a29c388d7eee83d3
mdboss2 <114162987+mdboss2@users.noreply.github.com>
2025-08-16 06:12:52 +0530

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-apt-get update && apt-get install -y wget tar
-wget -O N_m3u8DL-RE.tar.gz https://github.com/nilaoda/N_m3u8DL-RE/releases/latest/download/N_m3u8DL-RE_Linux_x64.tar.gz
-tar -xzf N_m3u8DL-RE.tar.gz
-chmod +x N_m3u8DL-RE
-mv N_m3u8DL-RE /usr/local/bin/
-pip install -r requirements.txt
